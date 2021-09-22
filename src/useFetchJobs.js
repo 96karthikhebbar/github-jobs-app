32f4b1877b8a8ab65f9fad9e8b0bd034991a337a
Karthik  Hebbar <96karthikhebbar@gmail.com>
2021-09-22 23:59:35 +0530

    updated useFetchJobs hook with reducer function and added state handling

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import { useReducer, useEffect } from 'react'
+
+const ACTIONS = {
+    MAKE_REQUEST : 'make-request',
+    GET_DATA: 'get-data',
+    ERROR: 'error'
+}
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case ACTIONS.MAKE_REQUEST:
+            return { loading:true ,jobs:[] }
+
+        case ACTIONS.GET_DATA:
+        return {...state, loading:false, jobs: action.payload.jobs }
+
+        case ACTIONS.ERROR:
+            return {...state, loading: false, error: action.payload.error, jobs:[]}
+
+        default:
+    }
+}
 
 const useFetchJobs = (params, page) => {
+
+    const[state, dispatch] = useReducer( reducer, { jobs: [], losding: true})
+
+    useEffect( () => {
+        dispatch({ type:ACTIONS.MAKE_REQUEST})
+    },[params, page])
+
+    
     return {
         jobs : [],
         loading : false,
